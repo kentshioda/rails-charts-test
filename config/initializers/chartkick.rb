07a3e243eb4b33f86dd7c418192e0edcbd617722
kent_s <esseesse500+member@gmail.com>
2018-08-16 21:15:27 +0900

    add chart.js

---
+++ config/initializers/chartkick.rb
@@ -0,0 +1,13 @@
+Chartkick.options = {
+  height: '400px',
+  library: {
+    layout: {
+      padding: {
+        left: 16,
+        right: 16,
+        top: 16,
+        bottom: 16
+      }
+    }
+  }
+}
