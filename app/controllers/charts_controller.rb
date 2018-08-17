5d1ef39c2f362f93b48b0cb417cbc1ce8980a213
kent_s <esseesse500+member@gmail.com>
2018-08-17 20:39:01 +0900

    fix chart

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,15 +1,30 @@
 class ChartsController < ApplicationController
   def index
-    places = []
+    @places = []
     places2count = RealEstate.all.group(:place).count
     places2count.each_key do |place|
-      places << place
+      @places << place
     end
-    @place2year_price = Hash.new { |h, k| h[k] = [] }
-    places.each do |place|
-      @year2price = RealEstate.where(place: place).group(:year).average(:price)
-      @year2price.each_pair do |year, price|
-        @place2year_price[place] << [year, price]
+    years = []
+    years2count = RealEstate.all.group(:year).count
+    years2count.each_key do |year|
+      years << year
+    end
+    @place_year_price_per_sm = Hash.new { |h, k| h[k] = [] }
+    @places.each do |place|
+      years.each do |year|
+        # @year2price = RealEstate.where(place: place).group(:year).average(:price)
+        prices_per_sm = []
+        place_year2info = RealEstate.select(:price, :square_meter).where(place: place, year: year)
+        place_year2info.each do |info|
+          prices_per_sm << info.price / info.square_meter
+        end
+        if prices_per_sm.size.zero? then
+          average_price = 0
+        else
+          average_price = prices_per_sm.sum / prices_per_sm.size
+        end
+        @place_year_price_per_sm[place] << [year, average_price]
       end
     end
   end
