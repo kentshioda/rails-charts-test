1277753dde1f3ed94ca884acd6892b9473ea65b9
kent_s <esseesse500+member@gmail.com>
2018-08-22 09:34:49 +0900

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,21 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 require 'csv'
 
 CSV.foreach('db/ppy.txt') do |info|
   RealEstate.create(
-
     place: info[0],
     price: info[1],
     year: info[2],
     square_meter: info[3]
+  )
+end
+
+CSV.foreach('db/ec_timedata.txt') do |info|
+  RecordNumber.create(
+    company: info[0],
+    date: info[1],
+    count: info[2]
   )
 end
